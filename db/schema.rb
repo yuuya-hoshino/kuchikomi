564c4490f35cc9a753032e2f32f875f565978673
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2021-10-30 12:12:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_115807) do
+ActiveRecord::Schema.define(version: 2021_10_30_024908) do
 
   create_table "agents", charset: "utf8", force: :cascade do |t|
     t.integer "agent_name"
